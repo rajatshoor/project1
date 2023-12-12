ebd3a394c92969ecb045d28223827b74c778cd89
rajatshoor <59229427+rajatshoor@users.noreply.github.com>
2023-12-11 23:40:48 -0500

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -1,19 +1,16 @@
-# AWS Provider configuration
-provider "aws" {
-  region = var.aws_region
-}
+# --- compute/main.tf ---
 
-# Data source for the latest Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
   owners = ["099720109477"] # Canonical owner ID for Ubuntu AMIs
 }
 
-# Launch template for the web instances
 resource "aws_launch_template" "web" {
   name_prefix            = "web"
   image_id               = data.aws_ami.ubuntu.id
@@ -26,7 +23,6 @@ resource "aws_launch_template" "web" {
   }
 }
 
-# Auto Scaling group for the web instances
 resource "aws_autoscaling_group" "web" {
   name                = "web"
   vpc_zone_identifier = tolist(var.public_subnet)
@@ -38,51 +34,4 @@ resource "aws_autoscaling_group" "web" {
     id      = aws_launch_template.web.id
     version = "$Latest"
   }
-}
-
-# CloudWatch metric alarm for high CPU utilization
-resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
-  alarm_name                = "high-cpu-utilization-${aws_launch_template.web.name}"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "2"
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/EC2"
-  period                    = "120"
-  statistic                 = "Average"
-  threshold                 = "75"
-  alarm_description         = "Alarm when CPU exceeds 75%"
-  treat_missing_data        = "missing"
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.web.name
-  }
-  actions_enabled           = true
-  alarm_actions             = [var.sns_topic_arn]
-  ok_actions                = [var.sns_topic_arn]
-  insufficient_data_actions = [var.sns_topic_arn]
-}
-
-# Variables (make sure to define these in your variables.tf or pass them directly)
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "web_instance_type" {
-  description = "Instance type for the web servers"
-  type        = string
-}
-
-variable "web_sg" {
-  description = "Security group for the web servers"
-  type        = list(string)
-}
-
-variable "public_subnet" {
-  description = "Public subnets for the web servers"
-  type        = list(string)
-}
-
-variable "sns_topic_arn" {
-  description = "SNS topic ARN for alarm actions"
-  type        = string
 }
