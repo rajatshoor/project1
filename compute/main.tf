ee5c67da36ba53365ea2197967f721b4c5249941
rajatshoor <59229427+rajatshoor@users.noreply.github.com>
2023-12-12 15:48:22 -0500

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -1,49 +1,24 @@
-# --- compute/main.tf ---
-
-provider "aws" {
-  region = "us-east-1" # or your preferred region
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical owner ID for Ubuntu AMIs
 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  owners = ["099720109477"] # Canonical owner ID for Ubuntu AMIs
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro" # or your preferred instance type
-  monitoring             = true       # Enable detailed monitoring
-  vpc_security_group_ids = ["sg-12345678"] # Replace with your security group ID
-  subnet_id              = "subnet-eddcdzz4" # Replace with your subnet ID
+resource "aws_launch_template" "web" {
+  name_prefix            = "web"
+  image_id               = data.aws_ami.ubuntu.id
+  instance_type          = var.web_instance_type
+  vpc_security_group_ids = [var.web_sg]
+  user_data              = filebase64("install_docker.sh")
 
   tags = {
-    Name = "web-instance"
+    Name = "web"
   }
-}
-
-resource "aws_cloudwatch_metric_alarm" "high_cpu" {
-  alarm_name          = "high-cpu-utilization"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "80"
-  alarm_description   = "This metric monitors ec2 cpu utilization"
-  actions_enabled     = true
-
-  dimensions = {
-    InstanceId = aws_instance.web.id
-  }
-
-  # Assuming you have an SNS topic for notifications
-  alarm_actions = ["arn:aws:sns:us-east-1:123456789012:my-sns-topic"]
 }
 
 resource "aws_autoscaling_group" "web" {
@@ -58,5 +33,3 @@ resource "aws_autoscaling_group" "web" {
     version = "$Latest"
   }
 }
-
-
