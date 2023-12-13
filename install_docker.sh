3049b45af7371657561de2253168cdd1a9f60530
rajatshoor <59229427+rajatshoor@users.noreply.github.com>
2023-12-12 20:15:26 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -30,6 +30,11 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 echo "Hello, World! from our test server." | sudo tee /var/www/html/index.html
 
+#!/bin/bash
+mkdir gcc_dir
+
+
+
 
              
 
